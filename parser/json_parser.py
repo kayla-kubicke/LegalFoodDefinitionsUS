69ba8ecbd4231e58c616d3723f3464b0767032c3
kayla-kubicke <161649341+kayla-kubicke@users.noreply.github.com>
2026-05-16 15:09:01 -0700

    Dynamically Selects Insert Method Based on Object Type

--- parser/json_parser.py
+++ parser/json_parser.py
@@ -28,69 +28,51 @@ class JSONParser:
 
 		return False
 
+	# https://www.youtube.com/watch?v=vO1CCXXMzBI
 	# Attempting to update search_results_dict(...) so I have the option to
 	# create a classic json or a dict with nested sets.
 	#
 	# The coupling with agencies_responsible_for_title_and_chapter(...) is a little
 	# worrisome.
-	# Obviously, test suite fails.
+	# UPDATE: test suite
 	class AuthorObjectType(Enum):
 		SET = 1
 		LIST = 2
 
-	# (!) Should this be a constant and/or moved to top of class?
-	# Yes.
-	# author_object_type_dict = {
-	# 	AuthorObjectType.SET: set,
-	# 	AuthorObjectType.LIST: []
-	# }
-
-	# Update the dict so I can id the correct method.
 	author_object_type_dict = {
-		AuthorObjectType.SET: { type: set, 'insert_method': 'add' },
+		AuthorObjectType.SET: { type: set(), 'insert_method': 'add' },
 		AuthorObjectType.LIST: { type: [], 'insert_method': 'append' }
 	}
-	# retrieved_function = getattr(type, insert_method)
 
 	# RETURNS an array of matching agencies, if empty no matching agencies
 	# were found
 	# ADD: id parent agency
 	# ADD: Testing
-	# https://www.youtube.com/watch?v=hiQgQCK8nn0
+	# UPDATE: Reduce repeat code
 	def agencies_responsible_for_title_and_chapter(title, chapter, author_object_type: AuthorObjectType = AuthorObjectType.SET): # Remove default?
 		agencies = example_handler.example_response('agency_data', 'agencies')
 		# 'Selects' returned_object's type.
-		# author_object = JSONParser.author_object_type_dict.get(author_object_type)
 		author_object = JSONParser.author_object_type_dict[author_object_type][type]
-		# Is this the best way to deal with this?
-		# Maybe it's time to add a constructor... Yeah.
-		if len(author_object) != 0:
-			# clear() works on both objects... thankfully.
-			author_object.clear()
 
-		# Ugh, no.
-		retrieved_function = getattr([], JSONParser.author_object_type_dict[author_object_type]['insert_method'])
-		# print(retrieved_function)
-		# print(getattr([], JSONParser.author_object_type_dict[author_object_type]['insert_method']).callable())
+		if len(author_object) != 0:
+			author_object.clear()
 
 		# Can't avoid nested for loops because of the json's structure.
 		for agency in agencies['agencies']:
 			if agency['children'] != []:
 				for child in agency['children']:
 					if JSONParser.title_and_chapter_found_in_agency_json(child, title, chapter):
-						author_object.append(child['short_name'])
-						# NO.
-						# author_object.retrieved_function(child['short_name'])
+						# Finally, whew.
+						getattr(author_object, JSONParser.author_object_type_dict[author_object_type]['insert_method'])(child['short_name'])
 			else:
 				if JSONParser.title_and_chapter_found_in_agency_json(agency, title, chapter):
-					author_object.append(agency['short_name'])
-					# NO.
-					# author_object.retrieved_function(agency['short_name'])
+					getattr(author_object, JSONParser.author_object_type_dict[author_object_type]['insert_method'])(child['short_name'])
 
 		if author_object == []:
 			author_object.append('unknown')
 
 		return author_object
+	# https://www.youtube.com/watch?v=G5eVBnOY-mI
 
 	# Generates dictionary with response results.
 	# UPDATE: RETURNS
@@ -140,4 +122,4 @@ class JSONParser:
 # END: JSONParser Class
 
 choco = example_handler.example_response('example_requests', 'chocolate')
-print(JSONParser.search_results_dict(choco, JSONParser.AuthorObjectType.LIST)) # , JSONParser.AuthorObjectType.LIST
+print(JSONParser.search_results_dict(choco)) # , JSONParser.AuthorObjectType.LIST
