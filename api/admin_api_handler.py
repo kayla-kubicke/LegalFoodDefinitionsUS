cbc9489634c40c0d7b1824227969f3e791ce4d45
kayla-kubicke <161649341+kayla-kubicke@users.noreply.github.com>
2026-07-18 18:52:59 -0700

    Added Logic for build_url(...)
    Untested.

--- api/admin_api_handler.py
+++ api/admin_api_handler.py
@@ -4,18 +4,15 @@ import api.api_constants as api_constants
 from api.api_handler import ApiHandler as api_handler
 from api.query_modifier import QueryModifier as query_modifier
 
-# ADD: Tests!!!
-
 class AdminApiHandler(api_handler):
 	# START: Enums
 	# .../v1/agencies.json
 	# .../v1/corrections.json
-	# .../v1/corrections/title/{title}.json #NOTE: Title is an int
+	# .../v1/corrections/title/{title}.json # NOTE: title is an int
 	class AdminType(str, Enum):
 		AGENCIES = 'agencies'
 		CORRECTIONS = 'corrections'
-		CORRECTIONS_TITLE = 'corrections/title'
-		DOT_JSON = '.json' # Do I need this?
+		CORRECTIONS_TITLE = 'corrections/title/'
 	# END: Enums
 
 	# START: Constructor
@@ -25,8 +22,52 @@ class AdminApiHandler(api_handler):
 	# END: Constructor
 
 	# START: Methods
-	def build_url(self, query):
-		return 'dummy value'
+	# Untested.
+	# title will only be used for CORRECTIONS_TITLE, if needed.
+	# RETURNS URL string
+	def build_url(self, title = ''):
+		# For AGENCIES and CORRECTIONS:
+		if (self.admin_type == 'agencies' or self.admin_type == 'corrections') and self.admin_type != 'corrections/title/':
+			# Spot check
+			return f'{api_constants.ECFR}/api/{self.service}/v1/{self.admin_type}.json'
 
+		# For CORRECTIONS_TITLE: Includes {title}
+		if isinstance(title, int):
+			title = str(title)
+
+		if self.admin_type == 'corrections/title/' and title == '':
+			raise ValueError('title param required for ...corrections/title/... url')
+
+		return f'{api_constants.ECFR}/api/{self.service}/v1/{self.admin_type}{title}.json'
+
+	# A search results call to api
+	# RETURNS
+	# Successful: dict object
+	# Unsuccessful:
+		# If response is returned but status code != 200:
+		# If an error is encountered during request:
 	def api_call(self, query):
 		return super().api_call(query)
+
+
+
+	# I need to prioritize whiteboard/data structure stuff.
+	# Consequetly, this project will be placed on the back burner.
+
+	#
+	# Use the following as needed.
+
+	# When the thoughts of your mistakes overwhelm you:
+		# https://www.youtube.com/watch?v=IP9TaFDe7qk
+
+	# When you feel like haunting your own home:
+		# https://www.youtube.com/watch?v=u9FguM9aAM8
+
+	# When the vapid hellscape begins to consume you:
+		# https://www.youtube.com/watch?v=qOM107PIxV8
+
+	# When you're accidentally the villian:
+		# https://www.youtube.com/watch?v=wlI_BgVvtQY
+
+	# When you start to think way too much about the Stranger Situation:
+		# https://www.youtube.com/watch?v=Hm3cf3jsWNw
